160a7f1f7bb92546d531c08931ed8e84e3154947
Marcelo Lopes Cairuga <cairuga@icloud.com>
2020-03-27 00:58:02 -0300

    backend + frontend + mobile

--- mobile/src/pages/Cases/index.js
+++ mobile/src/pages/Cases/index.js
@@ -1,22 +1,86 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
-
+import api from '../../services/api';
 
 export default function Cases() {
+    const navigation = useNavigation();
+    const [cases, setCases] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    function navigateToDetail(caso) {
+        navigation.navigate('Detail', {caso});
+    }
+
+    async function loadCases() {
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && cases.length == total) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('casos', {
+            params : { page }
+        });
+        setCases([... cases, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    }
+    useEffect(()=>{
+        loadCases();
+    }, [])
 
     return (
         <View style={styles.containter}>
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia!</Text>
+
+            <FlatList 
+                data={cases}
+                style={styles.caseList}
+                showsVerticalScrollIndicator="false" 
+                keyExtractor={caso => String(caso.id)}
+                onEndReached={loadCases}
+                onEndReachedThreshold={0.2}
+                renderItem={({item: caso})=>(
+                    <View style={styles.case}>
+                        <Text style={styles.caseProperty}>ONG: </Text>
+                        <Text style={styles.caseValue}>{caso.nome}</Text>
+
+                        <Text style={styles.caseProperty}>Caso: </Text>
+                        <Text style={styles.caseValue}>{caso.titulo}</Text>
+
+                        <Text style={styles.caseProperty}>Valor: </Text>
+                        <Text style={styles.caseValue}>
+                            {Intl.NumberFormat('pt-br', {style: 'currency',  currency:'BRL'}).format(caso.valor)}
+                        </Text>
+
+                        <TouchableOpacity 
+                            style={styles.detailButton}
+                            onPress = {() => navigateToDetail(caso)}
+                        >
+                            <Text style={styles.detailButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}            
+            />
         </View>
     );
 
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,10 +1,79 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, TouchableOpacity, Text, Linking } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
+
 
 export default function Detail() {
+    const navigation = useNavigation();
+    
+    const route = useRoute();
+    const caso = route.params.caso;
+    
+    const valor = Intl.NumberFormat('pt-br', {style: 'currency',  currency:'BRL'}).format(caso.valor);
+    const message = `Olá ${caso.nome}, estou entrando em contato porque gostaria de ajudar no caso ${caso.titulo} com o valor de ${valor}.`;
+    
+
+    
+    function navigateToCases() {
+        navigation.goBack();
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: `Herói do caso ${caso.titulo}`,
+            recipients: [caso.email],
+            body: message
+        })
+
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=${caso.whatsapp}&text=${message}`);
+    }
 
     return (
-        <View />
+        <View style={styles.container} >
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <TouchableOpacity 
+                    style={styles.detailButton}
+                    onPress = {navigateToCases}
+                >
+                    <Feather name="arrow-left" size={28} color="#e02041" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.case}>
+                <Text style={[styles.caseProperty, {marginTop: 0}]}>ONG: </Text>
+                <Text style={styles.caseValue}>{caso.nome} </Text>
+                <Text style={styles.caseValueSmall}>{caso.cidade} / {caso.uf}</Text>
+
+                <Text style={styles.caseProperty}>Caso: </Text>
+                <Text style={styles.caseValue}>{caso.titulo}</Text>
+
+                <Text style={styles.caseProperty}>Valor: </Text>
+                <Text style={styles.caseValue}>
+                    {Intl.NumberFormat('pt-br', {style: 'currency',  currency:'BRL'}).format(caso.valor)}
+                </Text>             
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     );
 
 }
